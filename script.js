532d8567c4ed99d80a6d9a019f8a0defeef78bec
Nathan Baird <nate19981@googlemail.com>
2022-09-18 19:26:32 +0100

    Basic GUI.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let operator;
+let operation = null;
+let firstTerm = '';
+let secondTerm = '';
+
+const NUMBER_BUTTONS = document.querySelectorAll('[data-number]');
+const OPERATOR_BUTTONS = document.querySelectorAll('data-operator');
+const DECIMAL_BUTTON = document.getElementById('decimalBtn');
+const EQUALS_BUTTON = document.getElementById('equalsBtn');
+const CLEAR_BUTTON = document.getElementById('clearBtn');
+const DELETE_BUTTON = document.getElementById('deleteBtn');
+const FIRST_TERM_SCREEN = document.getElementById('firstTermScreen');
+const SECOND_TERM_SCREEN = document.getElementById('secondTermScreen');
 
 function operate(operator, valueA, valueB) {
     switch(operator) {
