21334e8297a36e00c48e264132f02a5d30233ee7
Nathan Baird <nate19981@googlemail.com>
2022-09-18 15:53:57 +0100

    Basic calculator operations functionality added

---
+++ script.js
@@ -0,0 +1,15 @@
+function addition(valueA, valueB) {
+    return valueA + valueB;
+}
+
+function subtraction(valueA, valueB) {
+    return valueA - valueB;
+}
+
+function multiplication(valueA, valueB) {
+    return valueA * valueB;
+}
+
+function division(valueA, valueB) {
+    return valueA / valueB;
+}
