7041ecc20ac20ebdb70d10f81ba3f12efa7aabfe
Nathan Baird <nate19981@googlemail.com>
2022-09-18 17:07:10 +0100

    Operate function. Takes two numbers and performs a chosen operation

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-function addition(valueA, valueB) {
-    return valueA + valueB;
+let operator;
+
+function operate(operator, valueA, valueB) {
+    switch(operator) {
+        case "add":
+            addition(valueA, valueB);
+            break;
+        case "minus":
+            subtraction(valueA, valueB);
+            break;
+        case "multiply":
+            multiplication(valueA, valueB);
+            break;
+        case "divide":
+            division(valueA, valueB);
+            break;
+    }
+
 }
 
-function subtraction(valueA, valueB) {
-    return valueA - valueB;
+function addition(x, y) {
+    return x + y;
 }
 
-function multiplication(valueA, valueB) {
-    return valueA * valueB;
+function subtraction(x, y) {
+    console.log('sub works');
+    return x - y;
 }
 
-function division(valueA, valueB) {
-    return valueA / valueB;
+function multiplication(x, y) {
+    console.log('multi works');
+    return x * y;
+}
+
+function division(x, y) {
+    console.log('division works');
+    return x / y;
 }
